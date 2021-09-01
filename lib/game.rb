81cc86b8f2c5e9712c9f2f846abed14c8a6d1b4f
parentda <danielparent21@gmail.com>
2021-09-01 13:12:32 -0400

    added secret_Word function to Game class and filled out the initialize function

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,21 @@
-words = File.readlines('hangman_dictionary.txt', chomp: true)
+class Game
+  def initialize(lives, min_word_length, max_word_length)
+    @dictionary = File.readlines('hangman_dictionary.txt', chomp: true)
+    @min_word_length = min_word_length
+    @max_word_length = max_word_length
+    @lives_remaining = lives
+    @secret_word = secret_word
+    @turn_number = 1
+    @correct_guesses = []
+    @incorrect_guesses = []
+  end
 
-puts words.sample
+  def secret_word
+    while (word = @dictionary.sample)
+      return word if word.length.between?(@min_word_lenth, @max_word_length)
+    end
+  end
+end
 
-
+game = Game.new
+puts game.secret_word(5, 12)
