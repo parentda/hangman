176cc6024733dfc78c6d8445a76d28e4b191000e
parentda <danielparent21@gmail.com>
2021-09-02 13:26:38 -0400

    added functionality for updating output array and fleshed out the play method to properly run a full game

--- lib/game.rb
+++ lib/game.rb
@@ -6,8 +6,9 @@ class Game
     @min_word_length = min_word_length
     @max_word_length = max_word_length
     @lives_remaining = lives
-    @secret_word = random_word
+    @secret_word = random_word.upcase
     @turn_number = 1
+    @game_won = false
     @placeholder_char = '_'
     @correct_guesses = []
     @incorrect_guesses = []
@@ -31,14 +32,19 @@ class Game
 
   def valid_input?(string)
     if string.match?(/[A-Z]/) && string.length.eql?(1) &&
-         !@previous_guesses.include?(string)
+         !@correct_guesses.include?(string) &&
+         !@incorrect_guesses.include?(string)
       true
     else
       false
     end
   end
 
-  def update_output_array(letter); end
+  def update_output_array(letter)
+    (0...@secret_word.length)
+      .find_all { |idx| @secret_word[idx] == letter }
+      .each { |idx| @output_array[idx] = letter }
+  end
 
   def evaluate_guess(letter)
     if @secret_word.include?(letter)
@@ -52,20 +58,31 @@ class Game
     end
   end
 
-  def play; end
+  def play
+    intro_message
+    player_turn until @lives_remaining.zero? || game_won?
+    game_over_message(@game_won, @secret_word)
+  end
 
   def player_turn
+    display_output
     player_guess = player_input
     evaluate_guess(player_guess)
     @turn_number += 1
   end
 
   def display_output
-    puts @output_array.join(' ')
+    puts "
+    Turn: #{@turn_number}
+    Lives: #{@lives_remaining}
+    Incorrect Guesses: #{@incorrect_guesses.join(' ')}
+
+    #{@output_array.join(' ')}
+    "
   end
 
-  def game_over?
-    !@output_array.include?(@placeholder_char)
+  def game_won?
+    @game_won = true unless @output_array.include?(@placeholder_char)
   end
 
   def user_input_prompt
@@ -73,17 +90,34 @@ class Game
   end
 
   def warning_prompt
-    puts "\nSorry, the input must be an unused single alphabetical character. Please guess again: "
+    puts "\nSorry, that guess is invalid. Please try again and ensure the input is a single unused alphabetical character: "
   end
 
   def correct_guess_prompt(letter, occurances)
-    puts "\nGreat guess, there are #{occurances} #{letter}'s in the mystery word!"
+    if occurances == 1
+      puts "\nGreat guess, there is #{occurances} #{letter} in the mystery word!"
+    else
+      puts "\nGreat guess, there are #{occurances} #{letter}'s in the mystery word!"
+    end
   end
 
   def incorrect_guess_prompt(letter)
     puts "\nUnfortunately there are no #{letter}'s in the mystery word."
   end
+
+  def intro_message
+    puts 'Welcome to HANGMAN!'
+  end
+
+  def game_over_message(game_won, secret_word)
+    case game_won
+    when true
+      puts "\nCongrats, you won! The secret word was #{secret_word}"
+    when false
+      puts "\nSorry, you lost. The secret word was #{secret_word}"
+    end
+  end
 end
 
 @game = Game.new(6, 5, 12)
-puts @game.random_word
+# puts @game.random_word
