a5101f4fc547435834cf6a0290080597d1b63e6e
parentda <danielparent21@gmail.com>
2021-09-03 15:22:12 -0400

    minor refactoring to create a dedicated create_game function

--- lib/main.rb
+++ lib/main.rb
@@ -4,19 +4,22 @@ min_word_length = 5
 max_word_length = 12
 lives = 6
 
+def create_game
+  loop do
+    game_mode = Game.game_mode
+    if game_mode == '1'
+      puts Game.new_game_prompt
+      return Game.new(min_word_length, max_word_length, lives)
+    else
+      loaded_game = Game.open_file
+      return loaded_game unless loaded_game.nil?
+    end
+  end
+end
+
 loop do
   puts Game.intro_message(min_word_length, max_word_length, lives)
-  game =
-    loop do
-      game_mode = Game.game_mode
-      if game_mode == '1'
-        puts Game.new_game_prompt
-        break Game.new(min_word_length, max_word_length, lives)
-      else
-        loaded_game = Game.open_file
-        break loaded_game unless loaded_game.nil?
-      end
-    end
+  game = create_game
   game.play
   break game.end unless game.restart
 end
